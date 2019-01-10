ce6516bec9f58e4025d8a3162f77aafb546a1f65
liuwq <liuwq@tydic.com>
2019-01-10 15:49:14 +0800

    hehe

--- src/main/java/com/mayikt/util/OssUtil.java
+++ src/main/java/com/mayikt/util/OssUtil.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.UUID;
 
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.model.GetObjectRequest;
